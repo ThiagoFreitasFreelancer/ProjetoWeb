a7aa933438ea885458457ed1a49b1fa80b5b14e1
Thiago de Freitas <48700732+ThiagoFreitasFreelancer@users.noreply.github.com>
2024-09-16 11:25:12 -0300

    Delete BackEnd/src/Routes/login.routes.js

--- BackEnd/src/Routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const router = Router()
-const Login = require("../Controller/Login.controller");
-const login = new Login()
-
-//Rota de logar
-router.post("/login", async ( request, response ) => { await login.logar( request, response ) } );
-
-module.exports = router;
